ff8f1002ebda4bb0213747e376615541c995a17e
James Chen <jameschen09@yahoo.com>
2024-02-24 01:49:04 -0800

    yellow line animation

--- src/components/StatisticsBar.tsx
+++ src/components/StatisticsBar.tsx
@@ -13,15 +13,25 @@ interface StatisticsProp {
 const StatisticsAnimation: React.FC<StatisticsAnimationProps> = ({ value }) => {
     
     const width = (value / 1000) * 97.5;
-    
+    const yellowLineWidth = width - 15.5;
+
     return (
+    <>
     <motion.div 
         className='statistics-data'
         style={{width: `${width}%`}}
         initial={{ opacity: 0, scaleX: 0, transformOrigin: 'left' }}
         animate={{ opacity: 1, scaleX: 1 }} 
         transition={{ duration: 2, delay: 0.25, type: 'spring', stiffness: 30 }}
-    />
+        />
+        <motion.div 
+            className='yellow-line'
+            style={{ left: `${yellowLineWidth}%` }}
+            initial={{ opacity: 0, x: 25 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 1 }}
+        />
+    </>
     );
 };
 
