1e0cfc2abe304442ffaff48f9f25e2886f994b02
James Chen <jameschen09@yahoo.com>
2024-02-24 01:49:16 -0800

    data clean-up

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,34 @@
 import { proxy } from 'valtio';
 
 const store = proxy({
-    percentage: parseInt(localStorage.getItem('percentage') || '0') || 0, // If NaN set it to default 0
+    
+    percentage: parseFloat(localStorage.getItem('percentage') || '0') || 0, // If NaN set it to default 0
+    fakeBlocksData: [
+        {'value': 6},
+        {'value': 4.5},
+        {'value': 5},
+        {'value': 3},
+        {'value': 6},
+        {'value': 7},
+        {'value': 6.3},
+        {'value': 5},
+        {'value': 6},
+        {'value': 4.8},
+        {'value': 5},
+        {'value': 3},
+        {'value': 6},
+        {'value': 5.7},
+        {'value': 10},
+    ],
+    fakeBarsData: [
+        {'stat': 'Statistics', 'value' : 999 },
+        {'stat': 'Statistics', 'value' : 999 },
+        {'stat': 'Statistics', 'value' : 999 },
+    ]
 });
 
 export const setSliderPercentage = (value: number) => {
+    
     store.percentage = value;
     localStorage.setItem('percentage', value.toString());
 }
--- src/widgets/HeadshotStats.tsx
+++ src/widgets/HeadshotStats.tsx
@@ -10,47 +10,23 @@ import './styles/HeadshotStats.css'
 const HeadshotStats: React.FC = () => {
 
     const snap = useSnapshot(store);
-
-    const fakeBlocksData = [
-        {'value': 6},
-        {'value': 4.5},
-        {'value': 5},
-        {'value': 3},
-        {'value': 6},
-        {'value': 7},
-        {'value': 6.3},
-        {'value': 5},
-        {'value': 6},
-        {'value': 4.8},
-        {'value': 5},
-        {'value': 3},
-        {'value': 6},
-        {'value': 5.7},
-        {'value': 10},
-    ]
-
-    const fakeBarsData = [
-        {'stat': 'Statistics', 'value' : 999 },
-        {'stat': 'Statistics', 'value' : 500 },
-        {'stat': 'Statistics', 'value' : 209 },
-    ]
         
     return (
         <Card renderText={false}>
             <div className='circle-gauge-container'>
-                <div style={{ width: 155, height: 160 }}>
+                <div style={{ width: 150, height: 150 }}>
                     <CircleGauge value={store} />
                 </div>
                 <div>
                     <p className='header-category'>Headshot</p>
-                    <p className='header-stats'><span>{`${Math.floor(snap.percentage)}%`}</span> higher than your last 15 average</p>
+                    <p className='header-stats'><span>{`${Math.floor(snap.percentage)}%`}</span> higher than your last {store.fakeBlocksData.length} average</p>
                 </div>
             </div>
             <div>
-                <AverageBars data={fakeBlocksData} />
+                <AverageBars data={store.fakeBlocksData} />
             </div>
             <div>
-                <StatisticsBars data={fakeBarsData} />
+                <StatisticsBars data={store.fakeBarsData} />
             </div>
         </Card>
     );
